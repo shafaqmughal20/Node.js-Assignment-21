e8973ee6ec90ced5bf64b786cfab57bf49699428
shafaqmughal20 <160257641+shafaqmughal20@users.noreply.github.com>
2024-03-03 07:50:18 +0500

    Add files via upload

---
+++ Q-21.js
@@ -0,0 +1,8 @@
+"use strict";
+//21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let student1 = {
+    studentName: "shafaq",
+    studentSubject: "IT sudent",
+    studentAge: 27,
+};
+console.log(student1);
---
+++ Q-21.ts
@@ -0,0 +1,14 @@
+//21.They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+
+interface student1 {
+    studentName:string;
+    studentSubject:string;
+    studentAge:number;
+}
+let student1={
+    studentName :"shafaq",
+    studentSubject:"IT sudent",
+    studentAge: 27,
+};
+console.log(student1);
